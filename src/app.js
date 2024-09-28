30a046f8536793dd3383611853d23d49d963a68c
ashwinichavhan6291 <ashwinichavhan6291@gmail.com>
2024-09-28 23:25:01 +0530

    playing with code

--- src/app.js
+++ src/app.js
@@ -1,21 +1,9 @@
 const express = require("express");
 
 const app = express();
-app.use("/test/2", (req, res) => {
-  res.send("test1");
-});
-app.get("/user", (req, res) => {
-  res.send({ firstname: "ashwini", lastname: "chavhan" });
+
+app.get("/user/:userId/:name", (req, res) => {
+  console.log(req.params);
 });
 
-app.post("/user", (req, res) => {
-  res.send("data is saved in user post");
-});
-app.delete("/user", (req, res) => {
-  res.send("data is deleted successfully");
-});
-
-app.use("/", (req, res) => {
-  res.send("home page");
-});
 app.listen(3000);
