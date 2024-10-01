c61cfe68811739ba984acc51dbc34acaf32cb861
ashwinichavhan6291 <ashwinichavhan6291@gmail.com>
2024-10-01 14:45:26 +0530

    diving into the apis

--- src/app.js
+++ src/app.js
@@ -13,6 +13,52 @@ app.post("/signup", async (req, res) => {
   // res.send(req.body);
 });
 
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const users = await User.find({ emailId: userEmail });
+
+    if (users.length > 0) {
+      res.send(users);
+    } else {
+      res.status(400).send("user not found");
+    }
+  } catch (err) {
+    res.status(400).send("eroor");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("erorr");
+  }
+});
+
+app.get("/delete", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    const users = await User.findByIdAndDelete(userId);
+    // const users = await User.findOneAndDelete({ _id: userId });
+    res.send("user deleted successfully");
+  } catch (err) {
+    res.status(400).send("erorr");
+  }
+});
+
+app.get("/update", async (req, res) => {
+  const userId = req.body.userId;
+
+  const data = req.body;
+  console.log(data);
+  const users = await User.findByIdAndUpdate(userId, data);
+  res.send("user updated");
+});
 connectDB()
   .then(() => {
     console.log("database connected successfully..");
