f4ebb27c1c3adb4ea65cb77b6605d7961874f6fd
ashwinichavhan6291 <ashwinichavhan6291@gmail.com>
2024-10-05 10:08:28 +0530

    Authentication,JWT,Cookies

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 const { connectDB } = require("./config/database");
+const { userAuth } = require("./middlewares/auth.js");
 const User = require("./models/user");
-// console.log(__dirname);
+
 const { validateSignUpData } = require("./utils/validation.js");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
+app.use(cookieParser());
 app.post("/signup", async (req, res) => {
   try {
     validateSignUpData(req);
@@ -42,6 +47,13 @@ app.post("/login", async (req, res) => {
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (isPasswordValid) {
+      const token = await jwt.sign({ _id: user.id }, "devtinder@123", {
+        expiresIn: "1d",
+      });
+      // console.log(token);
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 8 * 3600000),
+      });
       res.send("login successfull");
     } else {
       throw Error("Invalid credentials");
@@ -51,6 +63,14 @@ app.post("/login", async (req, res) => {
   }
 });
 
+app.get("/profile", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("ERROR:" + err.message);
+  }
+});
 app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
 
@@ -115,6 +135,11 @@ app.get("/update/:userId", async (req, res) => {
   } catch (err) {
     res.status(400).send("update failed : " + err.message);
   }
+});
+
+app.post("/sendConnectionRequest", userAuth, async (req, res) => {
+  const user = req.user;
+  res.send(user.firstName + " sent a request");
 });
 connectDB()
   .then(() => {
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,13 +1,24 @@
-const adminAuth = (req, res, next) => {
-  console.log("auth is getting checked");
-  const token = "xyz";
-  const isAuthorized = token == "xyz";
-  if (!isAuthorized) {
-    res.status(401).send("unauthorized request");
-  } else {
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      throw new Error("token is not valid");
+    }
+    const decodedObj = await jwt.verify(token, "devtinder@123");
+    const { _id } = decodedObj;
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("user not found");
+    }
+    req.user = user;
     next();
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
   }
 };
 module.exports = {
-  adminAuth,
+  userAuth,
 };
