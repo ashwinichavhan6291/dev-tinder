242bdaa03c565c503f68b3406f8f807438e83e56
ashwinichavhan6291 <ashwinichavhan6291@gmail.com>
2024-10-05 11:00:22 +0530

    userSchema methods for jwt,bcrypt

--- src/app.js
+++ src/app.js
@@ -44,12 +44,11 @@ app.post("/login", async (req, res) => {
       throw Error("Invalid credentials");
     }
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+    const isPasswordValid = await user.validatePassword(password);
 
     if (isPasswordValid) {
-      const token = await jwt.sign({ _id: user.id }, "devtinder@123", {
-        expiresIn: "1d",
-      });
+      const token = await user.getJWT();
+
       // console.log(token);
       res.cookie("token", token, {
         expires: new Date(Date.now() + 8 * 3600000),
