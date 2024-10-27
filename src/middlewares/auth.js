26515e4882e71d715ee21fdf752c437eaa41c6e0
ashwinichavhan6291 <ashwinichavhan6291@gmail.com>
2024-10-27 19:30:11 +0530

    cors for front end connectivity

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,7 @@ const userAuth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
     if (!token) {
-      throw new Error("token is not valid");
+      return res.status(401).send("please Login!");
     }
     const decodedObj = await jwt.verify(token, "devtinder@123");
     const { _id } = decodedObj;
